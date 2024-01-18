485ce5046049026fd5269896f9c5a84c1e7e88f5
Aidan <aidanjh2012@gmail.com>
2024-01-18 22:36:05 +1100

    Created player movement and rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,64 +6,39 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 { 
     public float acceleration = 100f;
-    public float rotationSpeed = 1000f;
+    public float rotationSpeed = 10f;
     public float maxSpeed = 10f;
     public float deceleration = 5f;  // New field for deceleration
     private Vector2 currentVelocity;
     private Rigidbody2D rb;
-    private Gamepad gamepad;
+
     private Quaternion targetRotation;
     private bool isRotating;
 
-    private Generator generator;
-    private Shield shield;
-    private Engine engine;
+   
 
-    private List<Weapon> weapons;
-    public WeaponController WeaponController;
-
-
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
 
-        if (Gamepad.all.Count > 0)
-        {
-            gamepad = Gamepad.all[0];
-        }
     }
 
      void LateUpdate()
     {
-        if (gamepad == null)
-        {
-            
-        }
+        Vector2 direction = Gamepad.current.leftStick.ReadValue();
 
-        Vector2 direction = gamepad.leftStick.ReadValue();
-        UpdateTargetRotation(direction);
         UpdateVelocity(direction);
-        RotateShip();
-
-        
+        UpdateTargetRotation();
     }
 
-    private void UpdateTargetRotation(Vector2 direction)
+    private void UpdateTargetRotation()
     {
-        // Only update target rotation and set isRotating to true if joystick is being moved
-        if (direction.sqrMagnitude > 0)
-        {
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle - 90f));
-            isRotating = true;
-        }
-        else
-        {
-            isRotating = false;
-        }
+        float rotationAmount = Gamepad.current.rightTrigger.ReadValue() - Gamepad.current.leftTrigger.ReadValue();
+        transform.Rotate(Vector3.back, rotationAmount * rotationSpeed * Time.deltaTime);
+ 
     }
 
     private void UpdateVelocity(Vector2 direction)
