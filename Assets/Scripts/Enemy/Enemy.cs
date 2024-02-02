29e5f93d80326247d1901fee94f31cf185b62d52
Chi Ming Yip <cmyip99@gmail.com>
2024-02-02 23:51:58 +1100

    Moved rotation and movement to the base Enemy class

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,24 +2,37 @@ using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
 {
-	public float moveSpeed = 5f;
-	public float rotationSpeed = 3f;
-	protected Transform player;
-	public float health = 100f;
+	public float moveSpeed;
+	public float rotationSpeed;
+	public Transform player;
+	public float health;
+	public float collisionDamage;
 
 	protected virtual void Start()
 	{
-		player = GameObject.FindGameObjectWithTag("Player").transform;
-
-		if (player == null)
-		{
-			Debug.LogError("Player not found!");
-		}
+		SetPlayerReferenceByTag("Player");
 	}
 
 	protected virtual void Update()
 	{
-		
+		if (player != null)
+		{
+			RotateTowardsPlayer();
+			MoveTowardsPlayer();
+		}
+	}
+	
+	public void SetPlayerReferenceByTag(string playerTag)
+	{
+		GameObject playerObject = GameObject.FindGameObjectWithTag(playerTag);
+		if (playerObject != null)
+		{
+			player = playerObject.transform;
+		}
+		else
+		{
+			Debug.LogError("Player not found with tag: " + playerTag);
+		}
 	}
 	
 	void OnCollisionEnter2D(Collision2D collision)
@@ -27,7 +40,7 @@ public abstract class Enemy : MonoBehaviour
 		if(collision.gameObject.layer == LayerMask.NameToLayer("PlayerWeapons"))
 		{
 			Debug.Log("Player shot enemy: " + gameObject.name);
-			health -= 25f;
+			health -= collisionDamage;
 			if (health <= 0) 
 			{
 				Destroy(gameObject);  
@@ -35,5 +48,24 @@ public abstract class Enemy : MonoBehaviour
 		} else {
 			Debug.Log("The enemy has collided with" + collision.gameObject.name);
 		}	
+	}
+
+	void RotateTowardsPlayer()
+	{
+		// Direction vector from the enemy to the player
+		Vector2 direction = (player.position - transform.position).normalized;
+		// The difference in angle between the player and enemy
+		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+		// From this angle, calculate the target rotation
+		Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+		
+		// Smoothly transition between current rotation and the player's position
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+	}
+
+	void MoveTowardsPlayer()
+	{
+		// Moves the enemy towards the player
+		transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
 	}
 }
---
+++ Assets/Scripts/Enemy/ShootingEnemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : Enemy
+{
+	// Function for stopping at a certain distance away from the player
+	
+	// Function for shooting
+}
+
+
