c4fb85e0af7e030f44b9c53dee85ce222486e1a4
Chi Ming Yip <cmyip99@gmail.com>
2024-03-02 17:00:07 +1100

    WIP damageflash

--- Assets/Scripts/DamageFlash.cs
+++ Assets/Scripts/DamageFlash.cs
@@ -9,4 +9,31 @@ public class DamageFlash : MonoBehaviour
 	
 	private SpriteRenderer[] spriteRenderers;
 	private Material[] materials;
+	
+	private void Awake() 
+	{
+		SpriteRenderer parentSpriteRenderer = GetComponent<spriteRenderers>();
+		SpriteRenderer[] childrenSpriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		spriteRenderers = new SpriteRenderer[childrenSpriteRenderers.length + 1];
+		spriteRenderers[0] = parentSpriteRenderer;
+		
+		for (int i = 0; i < childrenSpriteRenderers.length; i++) 
+		{
+			spriteRenderers[i+1] = childrenSpriteRenderers[i];
+		}
+		
+		Init();
+	}
+	
+	private void Init() 
+	{
+		materials = new Material[spriteRenderers.length];
+		
+		for (int i = 0; i < spriteRenderers.length; i++) 
+		{
+			materials[i] = spriteRenderers[i].material;
+		}
+	}
+	
+	private IEnumerator 
 }
